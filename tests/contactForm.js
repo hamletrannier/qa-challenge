89f45a659740e9d851a661e2dee5e3ccab62b66a
hamletrannier <hamletr04@gmail.com>
2022-02-11 09:51:18 -0500

    Update contactForm.js
    test completed

--- tests/contactForm.js
+++ tests/contactForm.js
@@ -1,8 +1,26 @@
+import { Selector } from 'testcafe';
+
+const interfaceselect = Selector ('select#id_contact');
+const interfaceOption = interfaceselect.find('option')
+const emailAddress = Selector ("#email")
+const orderReference = Selector ("#id_order")
+const message = Selector ("#message")
+const btnSend = Selector ("#submitMessage")
+
+
 fixture("Contact Form")
-    .page("http://automationpractice.com");
+    .page("http://automationpractice.com/index.php?controller=contact");
 
-test ("First Test", async t =>{
+test ('contact Test', async t =>{
      await t 
+     .click(interfaceselect)
+     .click(interfaceOption.withText('Customer service'))
+     .typeText(emailAddress, "test@test.com")
+     .typeText(orderReference, "1234")
+     .typeText(message, "this is my first automation test")
+     .click(btnSend);
+     
 
 
-}) 
+
+});
