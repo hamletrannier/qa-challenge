decb7d3927c3b28038805d093ab27e53be16f6ed
hamletrannier <hamletr04@gmail.com>
2022-02-11 12:50:50 -0500

    updated

--- tests/contactForm.js
+++ tests/contactForm.js
@@ -4,23 +4,24 @@ const interfaceselect = Selector ('select#id_contact');
 const interfaceOption = interfaceselect.find('option')
 const emailAddress = Selector ("#email")
 const orderReference = Selector ("#id_order")
+const fileUpload = Selector ("#fileUpload")
 const message = Selector ("#message")
 const btnSend = Selector ("#submitMessage")
 
 
+// inject global configuration in the fixture context
+
 fixture("Contact Form")
     .page("http://automationpractice.com/index.php?controller=contact");
 
-test ('contact Test', async t =>{
+test ('Fill Contact Form', async t =>{
      await t 
      .click(interfaceselect)
      .click(interfaceOption.withText('Customer service'))
      .typeText(emailAddress, "test@test.com")
      .typeText(orderReference, "1234")
+     .setFilesToUpload(fileUpload, '../../upload/QA.jpg')
      .typeText(message, "this is my first automation test")
      .click(btnSend);
      
-
-
-
-});
+})
